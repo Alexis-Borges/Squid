4c93bebce50cc8fad0488bef1854a665f161472a
alexi <alexisborgesbarreiros@gmail.com>
2022-03-04 12:13:02 +0100

    -m

--- app/src/main/java/com/example/squid1/MainActivity.kt
+++ app/src/main/java/com/example/squid1/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.squid
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.squid.fragments.*
@@ -44,3 +42,5 @@ class MainActivity : AppCompatActivity() {
             commit()
         }
 }
+
+
